39e21e76f5f88b51234c83d1d4c3980151ec3b38
amandasf <souzaamandaf0@gmail.com>
2025-10-22 18:41:06 -0300

    Add files via upload

---
+++ gameJS-main/app.js
@@ -0,0 +1,30 @@
+alert("Boas vindas ao jogo do número secreto");
+let numeroMaximo = 150;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1); 
+let chute;
+console.log(numeroSecreto);
+let tentativas = 1
+
+// enquanto chute não for igual ao número secreto
+while (chute != numeroSecreto) {
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
+    // se chute for igual ao número secreto
+    if (numeroSecreto == chute) {
+        break;
+    } else {
+        if (numeroSecreto > chute) {
+            alert(`O número secreto é maior que ${chute}`);
+        } else {
+            alert(`O número secreto é menor que ${chute}`);
+        }
+        //tentativas = tentativas + 1;
+        tentativas++;
+    } 
+}
+let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa" ;
+alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
+//if (tentativas > 1) {
+        //alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+//} else  {
+        //alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa`);
+//}
